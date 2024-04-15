4898eb4e58fbc18f5c8bbfd6cac6d8281c8f5d6b
Đức <duc894580@gmail.com>
2024-04-15 22:46:25 +0700

    feat, refactor [footer, header] animated for header, refactor footer

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,14 @@ const burgerIcon = document.querySelector('.header__navbar-burger');
 const crossIcon = document.querySelector('.header__navbar-cross');
 
 const navbarDropdown = document.querySelector('.header__navbar-dropdown');
+const headerNavbar = document.querySelector('.header__navbar');
 
 burgerIcon.addEventListener('click', function(e) {
     e.stopPropagation();
     burgerIcon.classList.toggle('header__navbar-burger--hide'); 
     crossIcon.classList.toggle('header__navbar-cross--show'); 
     navbarDropdown.classList.toggle('header__navbar-dropdown--show');
+    headerNavbar.classList.toggle('header__navbar--mobile-bg');
 });
 
 crossIcon.addEventListener('click', function(e) {
@@ -16,6 +18,7 @@ crossIcon.addEventListener('click', function(e) {
     burgerIcon.classList.toggle('header__navbar-burger--hide'); 
     crossIcon.classList.toggle('header__navbar-cross--show'); 
     navbarDropdown.classList.toggle('header__navbar-dropdown--show');
+    headerNavbar.classList.toggle('header__navbar--mobile-bg');
 });
 
 
